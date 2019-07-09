38945dfc4fe605b0ce854a1bbe5e7c1171aae22a
Juan Sebastian Linares Torres <jlinares@redsis.com>
2019-07-09 11:04:13 -0500

    Se agrega archivo de propiedades para la conexion a base de datos

--- src/conexion/ConexionBD.java
+++ src/conexion/ConexionBD.java
@@ -1,10 +1,15 @@
 
 package conexion;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
@@ -18,11 +23,19 @@ public class ConexionBD {
   
     
      public static Connection connect(){
+         Properties prop = new Properties();
+         InputStream is = null;
         try {
-            String url = "jdbc:mysql://localhost:3306/controlherramientas?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+            is = new FileInputStream("./././propiedades.properties");
+            prop.load(is);
+           /* String url = "jdbc:mysql://localhost:3306/controlherramientas?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
             String user = "root";
-            String pass = "pass123";
+            String pass = "pass123";*/
+            String url = prop.getProperty("URL");
+            String user = prop.getProperty("USER");
+            String pass = prop.getProperty("PASS");
             System.out.println("Conectando...");
+            //System.out.println(url + "\n" + user + "\n" + pass);
             Connection connection = DriverManager.getConnection(url, user, pass);
             System.out.println("Conectado!!");
             
@@ -30,7 +43,11 @@ public class ConexionBD {
             return connection;
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Ocurrio un error conectando a la base de datos " + ex);
-        }
+        } catch (FileNotFoundException ex) {
+             Logger.getLogger(ConexionBD.class.getName()).log(Level.SEVERE, null, ex);
+         } catch (IOException ex) {
+             Logger.getLogger(ConexionBD.class.getName()).log(Level.SEVERE, null, ex);
+         }
         return null;
 
      }
